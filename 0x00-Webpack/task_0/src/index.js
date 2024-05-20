13e3bb678ee43544d4d2d931db9fee440adcb62a
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-05-20 23:10:14 +0100

    Update index.js

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,16 +1,7 @@
-// Import jQuery
 import $ from 'jquery';
 
-// Function to add paragraphs to the body
-function addParagraphs() {
-    // Add paragraphs to the body
-    $("body").append("<p>Dashboard data for the students</p>");
-    $("body").append("<p>Holberton Dashboard</p>");
-    $("body").append("<p>Copyright - Holberton School</p>");
-}
-
-// Call the function when the document is ready
-$(document).ready(function() {
-    addParagraphs();
+$(document).ready(function () {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
 });
-
