75b53491e7e9fed3f431d70329c44252d67acded
hyperayoub <bouagnaayoub@gmail.com>
2024-05-20 15:06:52 -0700

    webpack_start

---
+++ 0x00-Webpack/task_0/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development', // Set mode to 'development'
+    // Other webpack configuration options...
+};
+
