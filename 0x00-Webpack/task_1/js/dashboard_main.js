49c3d8284ff8462fd6e004e4c4f63aa0eba3cb5d
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-06-24 22:04:43 +0100

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,6 @@
 import $ from 'jquery';
 import _ from 'lodash';
-
-// Initialize counter //
-let count = 0;
-
-// updateCounter function //
-const updateCounter = () => {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-};
-
+// jquery here //
 $(document).ready(function(){
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
@@ -17,3 +8,12 @@ $(document).ready(function(){
     $('body').append(button);
     $('body').append('<p id="count"></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
+    let count = 0; // count start with 0 //
+
+    const updateCounter = () => {  // function with lodash //
+        count++;
+        $('#count').text(`${count} clicks on the button`);
+    };
+
+    $('#startBtn').on('click', _.debounce(updateCounter, 500));
+});  // end of teh code //
