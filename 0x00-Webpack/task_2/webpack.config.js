a2c6276c7c2babbb3b449624641cce545ae89677
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-06-24 23:13:49 +0100

    Update webpack.config.js

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -3,20 +3,29 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: './js/dashboard_main.js',
-    output: { 
-      path: path.resolve(__dirname, 'public'),
-      filename: 'bundle.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
     },
     module: {
-      rules: [ 
-        {
-          test: /\.css$/, 
-          use: ["style-loader", "css-loader"] 
-        },
-        { 
-          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-          type: 'asset/resource',
-        },
-      ],
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'assets',
+                            name: '[name].[ext]',
+                        },
+                    },
+                    'image-webpack-loader',
+                ],
+            },
+        ],
     },
 };
