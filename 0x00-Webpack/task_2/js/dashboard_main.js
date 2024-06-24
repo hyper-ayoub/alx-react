31c5ccda7f73044e65912fff1421161ae864b817
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-06-25 00:08:35 +0100

    Add files via upload

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').html(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
