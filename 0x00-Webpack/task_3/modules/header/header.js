b3a6dffde494b0fc918fa2442554e88bd7ad7059
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-06-25 11:20:36 +0100

    Delete 0x00-Webpack/task_3 directory

--- 0x00-Webpack/task_3/modules/header/header.js
+++
@@ -1,6 +0,0 @@
-import './header.css';
-import $ from 'jquery';
-
-$('body').append('<header></header>');
-$('header').append('<div id="logo"></div>', '<h1>Holberton Dashboard</h1>');
-console.log('Init header');
