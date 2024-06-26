30d8bc6ebe84a85497a270d8c3117022416d1397
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-06-26 21:37:55 +0100

    Create npm_install.sh

---
+++ 0x00-Webpack/npm_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# start your projet
+npm init -y
+
+# install webpack and webpack CLI 
+npm install webpack webpack-cli --save-dev
+
+# install css loader style loader
+npm install css-loader style-loader --save-dev
+
+# install file-loader
+npm install file-loader --save-dev
+
+# install jquery && lodash
+npm install jquery lodash --save
+
+# image-webpack-loader
+npm install image-webpack-loader --save-dev
+
+
+echo  " all Done ✅"
