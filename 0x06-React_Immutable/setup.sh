707daf8ac2746517342287220107c79058af8b12
hyperayoub <bouagnaayoub@gmail.com>
2024-07-16 13:13:11 -0700

    React Immutable

---
+++ 0x06-React_Immutable/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# The script below installs the above 2 at the specified versions
+
+ curl -sl https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+ sudo bash nodesource_setup.sh
+ sudo apt install nodejs
+
+ echo "Done🤩"
