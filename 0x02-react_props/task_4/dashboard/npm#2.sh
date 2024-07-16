1f94a31829b298d090fb77c0b94ad7073ec42fa9
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-07-17 00:10:37 +0100

    Proptypes + Unit Testing 🤩

--- 0x02-react_props/task_4/dashboard/npm#2.sh
+++ 0x02-react_props/task_4/dashboard/npm#2.sh
@@ -1,22 +1,12 @@
 #!/usr/env/bin bash
 
-
-
-
 # all npm for this  project
 npm install --save prop-types --legacy-peer-deps
 
 npm install react react-dom --legacy-peer-deps
 
 ## use this 
-
 npm install --save prop-types
 
 
-
-
-
-
-
-
-
+echo -e "done with this 🔍"
