a2fc275fe1e3a6cfe078812da8d5c187531d6ee1
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-07-19 01:05:47 +0100

    Update 3-list.js

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+import { List } from 'immutable';
+
+export function getListObject(array) {
+    return List(array);
+}
+export function addElementToList(list, element) {
+    return list.push(element);
+}
