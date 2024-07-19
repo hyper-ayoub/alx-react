9147183b58f584e99cfdbb7285b2f54bd9af5e3c
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-07-19 01:15:42 +0100

    Update 6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,8 +1,8 @@
 import { Map } from 'immutable';
 
-export default fucntion mergeDeeplyElements(page1, page2) {
-    const map1 = Map(page1);
-    const map2 = Map(page2);
+export default function mergeDeeplyElements(page1, page2) {
+  const m1 = Map(page1);
+  const m2 = Map(page2);
 
-    return map1.mergeDeep(map2);
+  return m1.mergeDeep(m2);
 }
