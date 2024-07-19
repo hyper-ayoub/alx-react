10cc51c1a121e55d66b4d0d523506b77b9f75953
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-07-19 01:06:28 +0100

    Update 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+import { Map, List } from 'immutable';
+
+export function concatedElements(page1, page2) {
+    const list1 = List(page1);
+    const list2 = List(page2);
+
+    return list1.concat(list2);
+}
+
+export function mergeElements(page1, page2) {
+    const map1 = Map(page1);
+    const map2 = Map(page2);
+
+    return map1.merge(map2);
+}
